4acb3140de7fb1057d0683763329b45adc908d9b
Jaszczab <23362384+jaszczab@users.noreply.github.com>
2018-10-21 14:25:08 +0200

    Solution Rebuild in work....

--- script.js
+++ script.js
@@ -2,55 +2,50 @@
 let buttonPOne = document.querySelector("#p1");
 let buttonPTwo = document.querySelector("#p2");
 let buttonReset = document.querySelector("#reset");
-let winingScore = document.querySelector("#getRed");
-let score = document.querySelector("#numberInput");
+let winningScore = document.querySelector("#getRed");
 /*===========Player Scores==============*/
 let p1Display = document.querySelector("#p1Display");
 let p2Display = document.querySelector("#p2Display");
+let pOneScore = "0";
+let pTwoScore = "0";
 let gameOver = false;
-let p1Win = NaN;
-winingScore.textContent = "5";
+let playerWin = NaN;
+winningScore.textContent = "5";
 /*--------Player 1----------*/
 buttonPOne.addEventListener("click",function setPOneScores() {
-    if (p1Display.textContent === winingScore.textContent) {
+    if (!gameOver) {
+        pOneScore++;
+        p1Display.textContent = pOneScore;
+    }else if (pOneScore === winningScore.textContent){
         gameOver = true;
-    }
-    if (!gameOver){
-        p1Display.textContent++;
-        console.log(p1Display);
-    }else {
-        p1Win = true;
+        playerWin = true;
         colorChange();
-        alert("Player One won");
     }
 });
 /*--------Player 2----------*/
 buttonPTwo.addEventListener("click",function setPTowScores() {
-    if (p2Display.textContent === winingScore.textContent) {
-        gameOver = true;
-    }
     if (!gameOver) {
-        p2Display.textContent++;
-        console.log(p2Display);
-    } else {
-        p1Win = false;
+        pTwoScore++;
+        p2Display.textContent = pTwoScore;
+    } else if (pTwoScore === winningScore.textContent){
+        gameOver = true;
+        playerWin = false;
         colorChange();
-        alert("Player Two won");
     }
 });
 /*----------Reset Button----------*/
 buttonReset.addEventListener("click",function resetScores() {
-    p1Display.textContent = "0";
-    p2Display.textContent = "0";
+    p1Display.textContent = pOneScore = "0";
+    p2Display.textContent = pTwoScore = "0";
     gameOver = false;
-    p1Win = NaN;
+    playerWin = NaN;
     p1Display.style.color = p2Display.style.color = "black";
 });
 /*----------Color Changer----------*/
 function colorChange() {
-    if (p1Win === true){
+    if (playerWin === true){
         p1Display.style.color = "rgba(6, 214, 160, 1)";
-    }else if (p1Win === false){
+    }else if (playerWin === false){
         p2Display.style.color = "rgba(6, 214, 160, 1)";
     }
 }
